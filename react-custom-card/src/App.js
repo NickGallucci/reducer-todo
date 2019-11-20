0773f53ab91815084ece0d8ab26c98d874f951b6
CryptoN999 <nicholasgallucci4@gmail.com>
2019-11-19 18:24:40 -0800

    got mvp adding styling

--- react-custom-card/src/App.js
+++ react-custom-card/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
 import './App.css';
+import {initialState, reducer} from "./reducers/reducer"
+import Form from "./components/Form"
+import Item from "./components/Item"
+
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <Form dispatch={dispatch}/>
+      <button onClick={() => dispatch({type: "removeCompleted"})}>Remove Completed</button>
+      {state.map(item => {
+        return <Item key={item.id} item={item} dispatch={dispatch}/>
+      })}
+
     </div>
   );
 }
